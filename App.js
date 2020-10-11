d95d9ed03911d9c582ad1830c94c1ee2cb3ff2a6
mrz-f0Th <72551171+mrz-f0Th@users.noreply.github.com>
2020-10-11 20:02:21 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+    	<div>
+    	  <h1>Halo Sahabat</h1>
+        
+    	  <button>Ini tombol React</button>
+    	  
+    	</div>
+    );
+  }
+}
+
+export default App;
